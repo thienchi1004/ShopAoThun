38f54401488e96f8e2c35133e871646b01291782
Huynh Thien Chi <thienchi1004@gmail.com>
2020-12-23 11:29:49 +0700

    first commit

---
+++ ShopAoThung/ViewModel/infoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ShopAoThung.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopAoThung.ViewModel
+{
+	public class infoViewModel
+	{
+		public List<Models.Cart_item> CartItems { get; set; }
+		public Muser Muser { get; set; }
+	}
+}
